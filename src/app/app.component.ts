a92ad4f7321e012900460fce2730a519c09c8eb8
Juanca9915 <jcamiloduque0215@gmail.com>
2023-06-22 23:28:52 -0500

    Se realizan cambios importantes y se agrega unas secciones a la aplicación de aprendizaje sobre la librería chart.js.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  sonGraficasNormalesVisibles: boolean;
+  elementosMenu: object[];
+
+  constructor() {}
+
+  ngOnInit(): void {
+      this.sonGraficasNormalesVisibles = true;
+      this.elementosMenu = [
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_self'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_self'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_self'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_self'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+      ];
+  }
+}
