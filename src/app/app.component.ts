7d6fd4127249ad50fb09b9b67fd500c7c6f89598
Juanca9915 <jcamiloduque0215@gmail.com>
2023-06-24 12:55:09 -0500

    Se realzan nuevos cambios.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // mostrarGrafica
   sonGraficasNormalesVisibles: boolean;
   elementosMenu: object[];
+  menuDesconocido = 'Menú Desconcido';
 
   constructor() {}
 
   ngOnInit(): void {
       this.sonGraficasNormalesVisibles = true;
       this.elementosMenu = [
-        {url: 'https://developer.mozilla.org/en-US/'},
-        {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
+        {url: '#graficaBarras', etiqueta: 'Barras'},
+        {url: '#graficaBarras2', etiqueta: 'Barras 2', tget: '_self'},
+        {url: '#graficaBarras2', etiqueta: 'Barras 2', tget: '_blank'},
         {url: 'https://developer.mozilla.org/en-US/', tget: '_self'},
         {url: 'https://developer.mozilla.org/en-US/', tget: '_blank'},
         {url: 'https://developer.mozilla.org/en-US/'},
